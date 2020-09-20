dc585cd9503d101c63a085dee633ba0a66a2e201
Kyle Bebak <kylebebak@gmail.com>
2020-09-20 13:10:34 -0500

    Redux v4

--- src/store.tsx
+++ src/store.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, useRef } from 'react'
+import React, { createContext, useContext, useState, useEffect } from 'react'
 
 export const createStore = (reducer, initialState = {}) => {
   let state = initialState
@@ -8,6 +8,7 @@ export const createStore = (reducer, initialState = {}) => {
     getState() {
       return state
     },
+    // Subscribe adds a listener to listeners, and returns unsubscribe method, which removes listener
     subscribe(listener) {
       listeners.push(listener)
 
@@ -17,6 +18,7 @@ export const createStore = (reducer, initialState = {}) => {
         listeners.splice(index, 1)
       }
     },
+    // Dispatch calls all listeners
     dispatch(action) {
       state = reducer(state, action)
 
@@ -31,17 +33,29 @@ const Context = createContext()
 
 export const Provider = ({ children, store }) => <Context.Provider value={store}>{children}</Context.Provider>
 
+/**
+ * Connect a component to store made available by Provider context, and ensure
+ * component rerenders whenever an action is dispatched to store to update its
+ * state.
+ *
+ * @param mapStateToProps - Function that maps Redux state to props passed in to
+ *     wrapped component whenever state is updated
+ *
+ * @returns Wrapped component that passes through props, plus props derived from
+ *     store tate
+ */
 export const connect = mapStateToProps => WrappedComponent => props => {
   const store = useContext(Context)
-  const [state, setState] = useState(() => mapStateToProps(store.getState(), props))
-  const propsRef = useRef()
-  propsRef.current = props
+  const [, forceUpdate] = useState(0)
+  const mappedState = mapStateToProps(store.getState(), props)
 
   useEffect(() => {
+    // For connected component, have listener just force wrapped component to
+    // re-render with new mapped props after dispatch is called
     return store.subscribe(() => {
-      setState(mapStateToProps(store.getState(), propsRef.current))
+      forceUpdate(c => c + 1)
     })
-  }, [store, setState, propsRef])
+  }, [store, forceUpdate])
 
-  return <WrappedComponent {...props} {...state} dispatch={store.dispatch} />
+  return <WrappedComponent {...props} {...mappedState} dispatch={store.dispatch} />
 }
